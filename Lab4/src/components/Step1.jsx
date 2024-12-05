91690c7cb23094a6f7d09e197d869df8315a6266
Anastasiialup <vt221_laa@student.ztu.edu.ua>
2024-12-05 23:19:41 +0200

    Added Lab4 simple components

---
+++ Lab4/src/components/Step1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Step1() {
+    return (
+        <div className="step-content">
+            <h2>Step 1: Welcome</h2>
+            <p>Welcome to the first step of our amazing journey!</p>
+        </div>
+    );
+}
+
+export default Step1;
---
+++ Lab4/src/components/Step2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Step2() {
+    return (
+        <div className="step-content">
+            <h2>Step 2: Details</h2>
+            <p>Please provide your details in this step.</p>
+        </div>
+    );
+}
+
+export default Step2;
